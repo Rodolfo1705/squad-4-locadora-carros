e1b14a9c98256c954f279ae994dbf4d80690756f
Rodolfo1705 <roabrahao5@gmail.com>
2023-08-15 23:29:37 -0300

    refact: some refact

--- src/main/java/org/system/LocadoraCarrosApplication.java
+++ src/main/java/org/system/LocadoraCarrosApplication.java
@@ -1,11 +1,10 @@
-package org.system.locadoracarros;
+package org.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LocadoraCarrosApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LocadoraCarrosApplication.class, args);
 	}
--- src/main/java/org/system/model/Sexo.java
+++ src/main/java/org/system/model/Sexo.java
@@ -1,16 +1,6 @@
 package org.system.model;
 
 public enum Sexo {
-    MASCULINO("Masculino"),
-    FEMININO("Feminino");
-
-    private final String sexo;
-
-    Sexo(String sexo) {
-        this.sexo = sexo;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
+    MASCULINO,
+    FEMININO;
 }
