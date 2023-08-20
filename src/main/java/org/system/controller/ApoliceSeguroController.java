240c39d28718a75f15d7e493cb068724505fc902
Rodolfo1705 <83622633+Rodolfo1705@users.noreply.github.com>
2023-08-20 00:03:19 -0300

    Update and rename ApolicesController.java to ApoliceSeguroController.java

--- src/main/java/org/system/controller/ApoliceSeguroController.java
+++ src/main/java/org/system/controller/ApoliceSeguroController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/apolicesSeguro")
-public class ApolicesController {
+public class ApoliceSeguroController {
     @Autowired
     private ApolicesServiceImpl apolicesService;
 
